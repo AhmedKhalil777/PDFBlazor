afcaf9dbb609796cfd30737664d288c2f8de5c85
Ahmed Khalil <ProgEng_Ahmed_khalil@outlook.com>
2021-05-29 17:12:04 +0200

    Add Canvases

--- src/PDFBlazor/PDFBlazor/wwwroot/pdfCreator.js
+++ src/PDFBlazor/PDFBlazor/wwwroot/pdfCreator.js
@@ -5,9 +5,13 @@ export function printPdf(id) {
     var divContents = document.getElementById(id).innerHTML;
     var links = document.getElementsByTagName('link');
     var hides = document.getElementsByClassName('pdf-hide');
+    var canvases = document.getElementsByTagName('canvas');
     for (var i = 0; i < hides.length; i++) {
         divContents =   divContents.replace(hides.item(i).outerHTML, '')
-    } 
+    }
+    for (var i = 0; i < canvases.length; i++) {
+        divContents = divContents.replace(canvases.item(i).outerHTML, "<br><img src='" + canvases.item(i).toDataURL() + "'/>")
+    }
     var styleElements = document.getElementsByTagName('style');
     var a = window.open('', '', 'height=1200, width=2000');
     a.document.write('<html><head>');
---
+++ src/Sample/Sample/wwwroot/canvas.js
@@ -0,0 +1,10 @@
+﻿setTimeout(canvasCreator, 5000);
+
+ function canvasCreator() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+}
+
